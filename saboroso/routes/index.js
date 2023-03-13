e12f4449a5c928d6e8420753c9bc72e5e741aecb
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-13 12:59:44 -0300

    Feat: Improved footer and header

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -4,20 +4,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
   conn.query(`
     SELECT * FROM tb_menus ORDER BY title
   
   `, (err, results) => {
     if(err){
       console.log(err);
+
     }
     else{
       res.render('index', { 
+        
         title: 'Restaurante Saboroso!',
         menus: results
        });
-
     }
   })
 
@@ -26,26 +26,33 @@ router.get('/', function(req, res, next) {
 
 router.get('/contacts', function(req, res, next){
   res.render('contacts', {
-    title: 'Contato - Restaurante Saboroso!'
+    title: 'Contato - Restaurante Saboroso!',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
   });
 })
 
 router.get('/menus', function(req, res, next){
   res.render('menus', {
-    title: 'Menu - Restaurante Saboroso!'
-
+    title: 'Menu - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
   });
 })
 
 router.get('/reservations', function(req, res, next){
   res.render('reservations', {
-    title: 'Reservas - Restaurante Saboroso!'
+    title: 'Reservas - Restaurante Saboroso!',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma mesa!'
   });
 })
 
 router.get('/services', function(req, res, next){
   res.render('services', {
-    title: 'Serviços - Restaurante Saboroso!'
+    title: 'Serviços - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer pode servir!'
   });
 })
 
