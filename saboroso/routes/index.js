f6a55a6aa7489713903d7cdda9325025dc676c2a
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-14 10:31:44 -0300

    Feat: Making reservation with post method for mysql

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -2,6 +2,7 @@ var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -30,12 +31,10 @@ router.get('/contacts', function(req, res, next){
 router.get('/menus', function(req, res, next){
 
   menus.getMenus().then(results =>{
-    res.render('menus', {
-      title: 'Menu - Restaurante Saboroso!',
-      background: 'images/img_bg_1.jpg',
-      h1: 'Saboreie nosso menu!',
-      menus: results
-    });
+
+    reservations.render(req, res);
+
+    
   });  
 });
 
@@ -43,8 +42,42 @@ router.get('/reservations', function(req, res, next){
   res.render('reservations', {
     title: 'Reservas - Restaurante Saboroso!',
     background: 'images/img_bg_2.jpg',
-    h1: 'Reserve uma mesa!'
+    h1: 'Reserve uma mesa!',
+    body: req.body
   });
+})
+
+router.post('/reservations', function(req, res, next){
+
+  if(!req.body.name){
+    reservations.render(req, res, "Digite o nome");
+  }
+  else if(!req.body.email){
+    reservations.render(req, res, "Digite o email");
+  }
+  else if(!req.body.people){
+    reservations.render(req, res, "Insira a quantidade de pessoas");
+  }
+  else if(!req.body.date){
+    reservations.render(req, res, "Digite a data");
+  }
+  else if(!req.body.time){
+    reservations.render(req, res, "Digite o horário");
+  }
+  else{
+    reservations.save(req.body).then(results =>{
+      
+      req.body = {};
+      
+      reservations.render(req, res, null, "Reserva realizada com suceso");
+
+
+    }).catch(err=>{
+      reservations.render(req, res, err.message);
+      
+    })
+  }
+
 })
 
 router.get('/services', function(req, res, next){
